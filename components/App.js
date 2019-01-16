27d1ea865e2570b15bbe6ca6687104d678c9506f
Tomasz Reizer <t_reizer@yahoo.com>
2019-01-16 18:25:19 +0100

    added changed files

--- components/App.js
+++ components/App.js
@@ -26,25 +26,23 @@ var contactForm = {
 };
 
 
-var App = React.createClass({
-  render: function() {
-    return (
-      React.createElement('div', {className: 'app'},
-        React.createElement(ContactForm, {contact: contactForm}),
-        React.createElement(Contacts, {items: contacts}, {})
-      )
-    );
-  }
-});
+//var App = React.createClass({
+//  render: function() {
+//    return (
+//      React.createElement('div', {className: 'app'},
+//        React.createElement(ContactForm, {contact: contactForm}),
+//        React.createElement(Contacts, {items: contacts}, {})
+//      )
+//    );
+//  }
+//});
 // ??????????????????????????????????????????????????????????????????????
 var App = React.createClass({
   render: function() {
     return (
       <div className={'app'}>
-      <ContactForm
-        React.createElement(ContactForm, {contact: contactForm}),
-        React.createElement(Contacts, {items: contacts}, {})
-
+        <ContactForm contact={contactForm}/>
+      <Contacts items={contacts}/>
       </div>
     );
   }
